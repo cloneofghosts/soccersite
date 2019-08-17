1530002ba395375367f85f40649ac64979589e34
Kev <thekpv8@gmail.com>
2019-08-17 17:14:29 -0400

    Added League, Division and Team Models
    - Started creatinng the db
    - Added a slug to the League
       - Need to hide the field on creation
    - Added a test league, team and division

--- lastever/admin.py
+++ lastever/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+
+from .models import League, Division, Team
+
+# Change Admin List Display
+class DivisionAdmin(admin.ModelAdmin):
+    list_display = ('division_name', 'division_leage')
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('team_name', 'team_abbreviation', 'team_division')
+    list_filter = ['team_division']
+    search_fields = ['team_name']
+
+class LeagueAdmin(admin.ModelAdmin):
+        list_display = ('league_name', 'slug')
+
 # Register your models here.
+admin.site.register(League, LeagueAdmin)
+admin.site.register(Division, DivisionAdmin)
+admin.site.register(Team, TeamAdmin)
--- lastever/models.py
+++ lastever/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Create your models here.
+class League(models.Model):
+    league_name = models.CharField(max_length=30)
+    slug = models.SlugField(blank=True)
+    def __str__(self):
+        return self.league_name
+    def save(self, *args, **kwargs):
+        if not self.id:
+            # Newly created object, so set slug
+            self.slug = slugify(self.league_name)
+
+        super(League, self).save(*args, **kwargs)
+
+class Division(models.Model):
+    division_name = models.CharField(max_length=30)
+    division_logo = models.URLField(null=True, blank=True)
+    division_leage = models.ForeignKey(League, on_delete=models.DO_NOTHING)
+    def __str__(self):
+        return self.division_name
+
+class Team(models.Model):
+    team_name = models.CharField(max_length=30)
+    team_abbreviation = models.CharField(max_length=3)
+    team_logo = models.URLField(null=True, blank=True)
+    team_division = models.ForeignKey(Division, on_delete=models.DO_NOTHING)
+    def __str__(self):
+        return self.team_name
