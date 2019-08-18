b49797c96319142131be6cfb69a307ab02b2fc08
Kev <thekpv8@gmail.com>
2019-08-17 22:18:24 -0400

    Add in Player, Schedule, News, Tags database
    - Made slugs not editable from admin page but viewable to the admins
    - Select2 is used whever there are foregin keys

--- lastever/admin.py
+++ lastever/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
-
-from .models import League, Division, Team
+from .models import League, Division, Team, Tags, Player, News, Schedule
 
 # Change Admin List Display
 class DivisionAdmin(admin.ModelAdmin):
     list_display = ('division_name', 'division_leage')
+    autocomplete_fields = ['division_leage']
 
 class TeamAdmin(admin.ModelAdmin):
     list_display = ('team_name', 'team_abbreviation', 'team_division')
@@ -13,9 +13,50 @@ class TeamAdmin(admin.ModelAdmin):
     search_fields = ['team_name']
 
 class LeagueAdmin(admin.ModelAdmin):
-        list_display = ('league_name', 'slug')
+    list_display = ('league_name', 'slug')
+    search_fields = ['league_name']
+
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'number', 'team')
+    list_filter = ['team']
+    search_fields = ['first_name', 'last_name', 'team']
+    autocomplete_fields = ['team']
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'posted', 'get_tags')
+    list_filter = ['posted']
+    search_fields = ['title']
+    autocomplete_fields = ['tags']
+
+    def get_tags(self, obj):
+        return "\n".join([t.tag_name for t in obj.tags.all()])
+
+    fieldsets = [
+        (None,               {'fields': ['title']}),
+        ('Date information', {'fields': ['posted'], 'classes': ['collapseable']}),
+        ('Tags',             {'fields': ['tags'], 'classes': ['collapseable']}),
+        ('Post',             {'fields': ['text']}),
+    ]
+
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display = ('scheduled_time', 'home_team', 'home_score', 'away_team', 'away_score', 'status', 'playoff', 'slug')
+    list_filter = ['scheduled_time', 'status', 'playoff']
+
+    fieldsets = [
+        ('Date information', {'fields': ['scheduled_time']}),
+        ('Team information', {'fields': ['home_team', 'home_score', 'away_team', 'away_score'], 'classes': ['collapseable']}),
+        ('Game Status',      {'fields': ['status', 'playoff']}),
+    ]
+    autocomplete_fields = ['home_team', 'away_team']
+
+class TagsAdmin(admin.ModelAdmin):
+    search_fields = ['tag_name']
 
 # Register your models here.
 admin.site.register(League, LeagueAdmin)
 admin.site.register(Division, DivisionAdmin)
 admin.site.register(Team, TeamAdmin)
+admin.site.register(Tags, TagsAdmin)
+admin.site.register(Player, PlayerAdmin)
+admin.site.register(News, NewsAdmin)
+admin.site.register(Schedule, ScheduleAdmin)
--- lastever/models.py
+++ lastever/models.py
@@ -4,7 +4,7 @@ from django.template.defaultfilters import slugify
 # Create your models here.
 class League(models.Model):
     league_name = models.CharField(max_length=30)
-    slug = models.SlugField(blank=True)
+    slug = models.SlugField(editable=False)
     def __str__(self):
         return self.league_name
     def save(self, *args, **kwargs):
@@ -28,3 +28,73 @@ class Team(models.Model):
     team_division = models.ForeignKey(Division, on_delete=models.DO_NOTHING)
     def __str__(self):
         return self.team_name
+
+class Player(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    number = models.PositiveSmallIntegerField(null=True, blank=True)
+    team = models.ForeignKey(Team, on_delete=models.DO_NOTHING)
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+class Tags(models.Model):
+    tag_name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.tag_name
+
+class News(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(editable=False)
+    posted = models.DateTimeField()
+    tags = models.ManyToManyField(Tags)
+    text = models.TextField()
+    def __str__(self):
+        return self.title
+    def save(self, *args, **kwargs):
+        if not self.id:
+            # Newly created object, so set slug
+            self.slug = slugify(self.title)
+
+        super(News, self).save(*args, **kwargs)
+
+class Schedule(models.Model):
+    # Options for Game Status
+    SCHEDULED = 'SCH'
+    FINAL = 'FIN'
+    FINAL_EXTRA_TIME = 'FET'
+    FINAL_PENALTYS = 'FPK'
+    CANCELLED = 'CAN'
+    RESCHEDULED = 'RES'
+    STATUS_CHOICES = [
+        (SCHEDULED, 'Scheduled'),
+        (FINAL, 'Final'),
+        (FINAL_EXTRA_TIME, 'Final/Extra Time'),
+        (FINAL_PENALTYS, 'Final/Penalty Kicks'),
+        (CANCELLED, 'Cancelled'),
+        (RESCHEDULED, 'Rescheduled'),
+    ]
+
+    scheduled_time = models.DateTimeField()
+    home_team = models.ForeignKey(Team, on_delete=models.DO_NOTHING, related_name='+')
+    home_score = models.PositiveSmallIntegerField(null=True, blank=True)
+    away_team = models.ForeignKey(Team, on_delete=models.DO_NOTHING, related_name='+')
+    away_score = models.PositiveSmallIntegerField(null=True, blank=True)
+    status = models.CharField(
+        max_length=3,
+        choices=STATUS_CHOICES,
+        default=SCHEDULED,
+    )
+    playoff = models.BooleanField()
+    slug = models.SlugField(editable=False)
+
+    def __str__(self):
+        str = "%s %s %s" % (self.home_team, ' vs ', self.away_team)
+        return str
+    def save(self, *args, **kwargs):
+        if not self.id:
+            # Newly created object, so set slug
+            slug_str = "%s %s %s" % (self.home_team, ' vs ', self.away_team)
+            self.slug = slugify(slug_str)
+
+        super(Schedule, self).save(*args, **kwargs)
