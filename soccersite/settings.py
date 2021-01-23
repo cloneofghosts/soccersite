b7470d24aa47b85bb3292546b58ce75464b73189
Kev <thekpv8@gmail.com>
2021-01-23 18:24:36 -0500

    Fix standings bug and remove Jet
    Jet is broken so has been removed from the admin site so the project will run now

--- soccersite/settings.py
+++ soccersite/settings.py
@@ -33,8 +33,6 @@ SITE_ID = 1
 # Application definition
 
 INSTALLED_APPS = [
-    'jet.dashboard',
-    'jet',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -196,42 +194,6 @@ TINYMCE_DEFAULT_CONFIG = {
     'height': 360,
 }
 TINYMCE_SPELLCHECKER = True
-
-
-JET_THEMES = [
-    {
-        'theme': 'default', # theme folder name
-        'color': '#47bac1', # color of the theme's button in user menu
-        'title': 'Default' # theme title
-    },
-    {
-        'theme': 'green',
-        'color': '#44b78b',
-        'title': 'Green'
-    },
-    {
-        'theme': 'light-green',
-        'color': '#2faa60',
-        'title': 'Light Green'
-    },
-    {
-        'theme': 'light-violet',
-        'color': '#a464c4',
-        'title': 'Light Violet'
-    },
-    {
-        'theme': 'light-blue',
-        'color': '#5EADDE',
-        'title': 'Light Blue'
-    },
-    {
-        'theme': 'light-gray',
-        'color': '#222',
-        'title': 'Light Gray'
-    }
-]
-
-JET_SIDE_MENU_COMPACT = True
 
 # Allauth custom signup form
 ACCOUNT_SIGNUP_FORM_CLASS = 'lastever.forms.AllauthSignupForm'
--- soccersite/urls.py
+++ soccersite/urls.py
@@ -45,8 +45,6 @@ urlpatterns = [
         name='password_reset_complete',
     ),
     path('tinymce/', include('tinymce.urls')),
-    path('jet/', include('jet.urls', 'jet')),  # Django JET URLS
-    path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),  # Django JET dashboard URLS
     path('accounts/', include('allauth.urls')),
     path('forum/', include(machina_urls)),
     url(r'^', include('allauth_2fa.urls')),
